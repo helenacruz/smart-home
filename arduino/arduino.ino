0845de805710a4d8ce9cdbb84f5d27d1dd6b28bf
Helena Cruz <Helena Cruz>
2017-03-20 21:09:23 +0000

    more sensor testing and readings more stable. in case of fire, buzzer goes off

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,47 +1,157 @@
 #define DEBUG 1
+#define READINGS 99
 
-int tempPin = A5;
-int lightPin = A3;
+const int temp_pin = A0;
+const int light_pin = A4;
+const int flame_pin = 12;
 
-unsigned int minLight = 1023;
-unsigned int maxLight = 0;
+const int yellow_button = 2;
+const int blue_button = 3;
 
-int photocellReading;
+const int blue_led = 7;
+const int yellow_led = 5;
+
+const int buzzer = 11;
+
+bool fire;
+bool buzzer_on;
+
+unsigned long buzzer_millis;
+
+int temp_readings;
+float temp_avg;
+
+int light_readings;
+int light_avg;
+
+int flame_readings;
+int flame_avg;
 
 void setup()
 {
   if (DEBUG) {
     Serial.begin(9600);
   }
+  pinMode(temp_pin, INPUT);
+  pinMode(light_pin, INPUT);
+  pinMode(flame_pin, INPUT);
+  pinMode(yellow_button, INPUT);
+  pinMode(blue_button, INPUT);
+  pinMode(blue_led, OUTPUT);
+  pinMode(yellow_led, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+
+  fire = false;
+  buzzer_on = false;
+  buzzer_millis = millis();
+
+  temp_readings = 0;
+  temp_avg = 0;
+
+  light_readings = 0;
+  temp_avg = 0;
+
+  flame_readings = 0;
+  flame_avg = 0;
 }
 
 void loop()
 {
-  readTemperature();
-  delay(1000);
-  readLight();
-  delay(1000);
+  read_temperature();
+  read_light();
+  read_flame();
+  if (fire) {
+    play_buzzer();
+  }
 }
 
-void readTemperature()
+void read_temperature()
 {
-  int val = analogRead(tempPin);
+  int val = analogRead(temp_pin);
   float mv = (val / 1024.0) * 5000; 
   float temperature = mv/10;
 
-  if (DEBUG) {
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println(" *C");
+  if (temp_readings < READINGS) {
+    temp_avg = (temp_avg * temp_readings + temperature) / ++temp_readings;
+  }
+  else {
+    temp_readings = 0;
+    if (DEBUG) {
+      Serial.print("Temperature: ");
+      Serial.print(temp_avg);
+      Serial.println(" *C");
+    }
   }
 }
 
-void readLight() 
+void read_light() 
 {
-   photocellReading = analogRead(lightPin);
+  int light = analogRead(light_pin);
 
-  if (DEBUG) {
-    Serial.print("Light = ");
-    Serial.println(photocellReading);
+  if (light_readings < READINGS) {
+    light_avg = (light_avg * light_readings + light) / ++light_readings;
+  }
+  else {
+    light_readings = 0;
+    if (DEBUG) {
+      Serial.print("Light = ");
+      Serial.println(light);
+    }
   }
 }
+
+void read_flame()
+{
+  int flame = digitalRead(flame_pin);
+
+  if (flame_readings < READINGS) {  
+    if (flame == LOW) {
+      flame_avg++;
+    }
+    else {
+      flame_avg--;
+    }
+    flame_readings++;
+  }
+  else {
+    if (flame_avg >= 60) {
+      if (DEBUG) {
+        Serial.println("FIRE");
+      }
+      fire = true;
+    }
+    else {
+      if (DEBUG) {
+        Serial.println("No fire");
+      }
+      fire = false;
+      noTone(buzzer);
+    }
+    flame_readings = 0;
+    flame_avg = 0;
+  }
+}
+
+void play_buzzer()
+{
+  unsigned long current_millis = millis();
+
+  if (current_millis - buzzer_millis >= 1000) {
+    buzzer_millis = current_millis;
+    if (buzzer_on == true) {
+      noTone(buzzer);
+      buzzer_on = false;
+      if (DEBUG) {
+        Serial.println("Buzzer off");
+      }
+    }
+    else {
+      tone(buzzer, 1000);
+      buzzer_on = true;
+      if (DEBUG) {
+        Serial.println("Buzzer on");
+      }
+    }
+  }
+}
+
